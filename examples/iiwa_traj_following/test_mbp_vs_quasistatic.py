c76715245a2033d8961361148fbd0cf15b7f8429
Unknown <pangtao@mit.edu>
2021-01-03 18:01:54 -0500

    Add doc string explaining the unittest.

--- examples/iiwa_traj_following/test_mbp_vs_quasistatic.py
+++ examples/iiwa_traj_following/test_mbp_vs_quasistatic.py
@@ -4,12 +4,20 @@ from .run_mbp_vs_quasistatic import *
 
 class TestIiwaTrajectoryFollowing(unittest.TestCase):
     def test_traj_following_accuracy(self):
+        """
+        For reasons I don't yet understand, the integral error between
+         MBP-simulated trajectories and the commanded trajectories converge
+         to a non-zero value, even with long trajectory duration (100s) and
+         small MBP time step (1e-5).
+
+        On the otherhand, integral error of quasistatic-simulated
+         trajectories approaches 0 as h_quasistatic decreases.
+        """
         q_iiwa_log_mbp, t_mbp, q_iiwa_log_quasistatic, t_quasistatic = \
             run_comparison(is_visualizing=False, real_time_rate=0.0)
 
         # convert q_gt_knots to a piecewise polynomial.
         shift_q_traj_to_start_at_minus_h(q_iiwa_traj, 0)
-        q_mbp_traj = PiecewisePolynomial.FirstOrderHold(t_mbp, q_iiwa_log_mbp.T)
 
         # MBP vs commanded.
         e2, e_vec2, t_e2 = calc_error_integral(
