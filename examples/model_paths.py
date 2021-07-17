94d3c4f6e814f99f9d79caef1fd89ef10bdbe299
Unknown <pangtao@mit.edu>
2021-07-16 23:33:19 -0400

    Rename models package from 'local' to 'quasistatic_simulator'.

--- examples/model_paths.py
+++ examples/model_paths.py
@@ -25,7 +25,7 @@ def add_package_paths_local(parser: Parser):
         os.path.join(pydrake.common.GetDrakePath(),
                      "manipulation/models"))
 
-    parser.package_map().Add("local", models_dir)
+    parser.package_map().Add("quasistatic_simulator", models_dir)
 
     parser.package_map().Add(
         'iiwa_controller', os.path.join(get_package_path(), 'models'))
