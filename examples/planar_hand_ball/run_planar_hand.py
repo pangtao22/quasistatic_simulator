89f40c7cf3b148781ca7fe5a1df645de851fd13a
Unknown <pangtao@mit.edu>
2021-07-22 17:15:30 -0400

    Revive planar hand sphere example.

--- examples/planar_hand_ball/run_planar_hand.py
+++ examples/planar_hand_ball/run_planar_hand.py
@@ -1,51 +1,37 @@
+import os
 import numpy as np
 
 from pydrake.all import RigidTransform, DiagramBuilder, PiecewisePolynomial
 
 from examples.setup_simulation_diagram import (
     run_quasistatic_sim, shift_q_traj_to_start_at_minus_h)
-from contact_mode_cluster.first_file import robot_sdf_paths, object_sdf_path
-from core.quasistatic_simulator import (
-    RobotInfo, QuasistaticSimParameters, create_plant_with_robots_and_objects)
+from core.quasistatic_simulator import QuasistaticSimParameters
+from examples.model_paths import models_dir
 
-#%% sim params
+object_sdf_path = os.path.join(models_dir, "sphere_yz_rotation_r_0.25m.sdf")
+model_directive_path = os.path.join(models_dir, "planar_hand.yml")
+
+#%% sim setup
 h = 0.2
 T = int(round(2 / h))  # num of time steps to simulate forward.
 duration = T * h
-sim_settings = QuasistaticSimParameters(is_quasi_dynamic=True,
-                                        is_unconstrained=True,
-                                        log_barrier_weight=100,
-                                        time_step=h,
-                                        contact_detection_tolerance=1.0)
-gravity = np.array([0, 0, -10.])
+sim_settings = QuasistaticSimParameters(
+    gravity=np.array([0, 0, -10.]),
+    nd_per_contact=2,
+    contact_detection_tolerance=1.0,
+    is_quasi_dynamic=True)
 
-#%%
 # robots.
 Kp = np.array([50, 25], dtype=float)
-robot_l_name = "robot_left"
-X_WL = RigidTransform()
-X_WL.set_translation([0, -0.1, 2])
-robot_l_info = RobotInfo(
-    sdf_path=robot_sdf_paths[0],
-    parent_model_name="WorldModelInstance",
-    parent_frame_name="WorldBody",
-    base_frame_name="link_0",
-    X_PB=X_WL,
-    joint_stiffness=Kp)
+robot_l_name = "arm_left"
+robot_r_name = "arm_right"
+robot_stiffness_dict = {robot_l_name: Kp, robot_r_name: Kp}
 
-robot_r_name = "robot_right"
-X_WR = RigidTransform()
-X_WR.set_translation([0, 0.1, 2])
-robot_r_info = RobotInfo(
-    sdf_path=robot_sdf_paths[1],
-    parent_model_name="WorldModelInstance",
-    parent_frame_name="WorldBody",
-    base_frame_name="link_0",
-    X_PB=X_WR,
-    joint_stiffness=Kp)
+# object
+object_name = "sphere"
+object_sdf_dict = {object_name: object_sdf_path}
 
-robot_info_dict = {robot_l_name: robot_l_info, robot_r_name: robot_r_info}
-
+# trajectory and initial conditions.
 nq_a = 2
 qa_l_knots = np.zeros((2, nq_a))
 qa_l_knots[0] = [-np.pi / 4, -np.pi / 4]
@@ -59,12 +45,9 @@ q_robot_r_traj = PiecewisePolynomial.ZeroOrderHold(
 
 q_a_traj_dict_str = {robot_l_name: q_robot_l_traj,
                      robot_r_name: q_robot_r_traj}
-# object
-object_name = "sphere"
-object_sdf_dict = {object_name: object_sdf_path}
-q_u0 = np.array([0, 2.35, 0])
 
-# initial conditions dict.
+q_u0 = np.array([0, 0.35, 0])
+
 q0_dict_str = {object_name: q_u0,
                robot_l_name: qa_l_knots[0],
                robot_r_name: qa_r_knots[0]}
@@ -73,16 +56,15 @@ q0_dict_str = {object_name: q_u0,
 #%% run sim.
 if __name__ == "__main__":
     loggers_dict_quasistatic_str, q_sys = run_quasistatic_sim(
+        model_directive_path=model_directive_path,
+        object_sdf_paths=object_sdf_dict,
         q_a_traj_dict_str=q_a_traj_dict_str,
         q0_dict_str=q0_dict_str,
-        robot_stiffness_dict=robot_info_dict,
-        object_sdf_paths=object_sdf_dict,
+        robot_stiffness_dict=robot_stiffness_dict,
         h=h,
-        gravity=gravity,
+        sim_params=sim_settings,
         is_visualizing=True,
-        real_time_rate=1.0,
-        sim_settings=sim_settings,
-        nd_per_contact=2)
+        real_time_rate=1.0)
 
 #%% look into the plant.
     plant = q_sys.plant
@@ -112,7 +94,7 @@ if __name__ == "__main__":
 
     v_log[1:, :] = (q_log[1:, :] - q_log[:-1, :]) / h
 
-    for name in robot_info_dict.keys():
+    for name in robot_stiffness_dict.keys():
         model = name_to_model_dict[name]
         logger_qa = loggers_dict_quasistatic_str[name]
         idx_v = q_sys.q_sim.velocity_indices_dict[model]
