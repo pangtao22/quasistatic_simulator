1a026d62da7d01b0818f5f296ad0a6af0d0604c8
Unknown <pangtao@mit.edu>
2022-03-25 17:30:41 -0700

    Revive planar two spheres example.

--- examples/two_spheres_yz/run_planar_pushing.py
+++ examples/two_spheres_yz/run_planar_pushing.py
@@ -4,31 +4,23 @@ import numpy as np
 from pydrake.all import PiecewisePolynomial
 
 from examples.setup_simulations import run_quasistatic_sim
-from qsim.simulator import QuasistaticSimParameters
+from qsim.parser import QuasistaticParser, QuasistaticSystemBackend
+from qsim.simulator import ForwardDynamicsMode, GradientMode
 from qsim.model_paths import models_dir
 
 object_sdf_path = os.path.join(models_dir, "sphere_yz.sdf")
 model_directive_path = os.path.join(models_dir, "sphere_yz_actuated.yml")
 
 #%% sim setup
-h = 0.05
+q_model_path = os.path.join(models_dir, 'q_sys', 'two_spheres_yz.yml')
+
+h = 0.1
 T = int(round(2 / h))  # num of time steps to simulate forward.
 duration = T * h
-quasistatic_sim_params = QuasistaticSimParameters(
-    gravity=np.array([0, 0, 0.]),
-    nd_per_contact=2,
-    contact_detection_tolerance=np.inf,
-    is_quasi_dynamic=True,
-    requires_grad=True)
 
 # robot
-Kp = np.array([100, 100], dtype=float)
 robot_name = "sphere_yz_actuated"
-robot_stiffness_dict = {robot_name: Kp}
-
-# object
 object_name = "sphere_yz"
-object_sdf_dict = {object_name: object_sdf_path}
 
 # trajectory and initial conditions.
 nq_a = 2
@@ -44,44 +36,40 @@ q0_dict_str = {object_name: qu0,
                robot_name: qa_knots[0]}
 
 
-#%% run sim.
-if __name__ == "__main__":
-    loggers_dict_quasistatic_str, q_sys = run_quasistatic_sim(
-        model_directive_path=model_directive_path,
-        object_sdf_paths=object_sdf_dict,
-        q_a_traj_dict_str=q_a_traj_dict_str,
-        q0_dict_str=q0_dict_str,
-        robot_stiffness_dict={robot_name: Kp},
-        h=h,
-        sim_params=quasistatic_sim_params,
-        is_visualizing=True,
-        real_time_rate=1.0)
+q_parser = QuasistaticParser(q_model_path)
+q_parser.set_sim_params(
+    h=h,
+    is_quasi_dynamic=True,
+    forward_mode=ForwardDynamicsMode.kQpMp,
+    log_barrier_weight=100)
+
+# loggers_dict_quasistatic_str, q_sys = run_quasistatic_sim(
+#     q_parser=q_parser,
+#     backend=QuasistaticSystemBackend.CPP,
+#     q_a_traj_dict_str=q_a_traj_dict_str,
+#     q0_dict_str=q0_dict_str,
+#     is_visualizing=True,
+#     real_time_rate=1.0)
+
+q_sim = q_parser.make_simulator_cpp()
+name_to_model_dict = q_sim.get_model_instance_name_to_index_map()
+idx_a = name_to_model_dict[robot_name]
+idx_u = name_to_model_dict[object_name]
+q0_dict = {idx_a: qa_knots[0], idx_u: qu0}
+
+# analytic gradient
+sim_params = q_sim.get_sim_params()
+sim_params.gradient_mode = GradientMode.kAB
+
+#%%
+q_sim.update_mbp_positions(q0_dict)
+tau_ext_dict = q_sim.calc_tau_ext([])
+q_sim.step(q_a_cmd_dict=q0_dict,
+           tau_ext_dict=tau_ext_dict,
+           sim_params=sim_params)
+
+A = q_sim.get_Dq_nextDq()
+B = q_sim.get_Dq_nextDqa_cmd()
 
 
-#%% look into the plant.
-    plant = q_sys.plant
-    for model in q_sys.q_sim.models_all:
-        print(model, plant.GetModelInstanceName(model),
-              q_sys.q_sim.velocity_indices[model])
-
-
-#%% construct q and v vectors of MBP from log.
-    name_to_model_dict = q_sys.q_sim.get_model_instance_name_to_index_map()
-    logger_qa = loggers_dict_quasistatic_str[robot_name]
-    logger_qu = loggers_dict_quasistatic_str[object_name]
-    q_log = np.zeros((T, plant.num_positions()))
-    v_log = np.zeros((T, plant.num_velocities()))
-    tau_a_log = np.zeros((T - 1, plant.num_actuated_dofs()))
-
-    for name, logger in loggers_dict_quasistatic_str.items():
-        model = name_to_model_dict[name]
-        for i, j in enumerate(q_sys.q_sim.velocity_indices[model]):
-            q_log[:, j] = logger.data().T[:, i]
-
-    v_log[1:, :] = (q_log[1:, :] - q_log[:-1, :]) / h
-
-    for l in range(T - 1):
-        qa_l = logger_qa.data().T[l]
-        qa_l1_cmd = qa_traj.value((l + 1) * h).squeeze()
-        tau_a_log[l] = Kp * (qa_l1_cmd - qa_l)
 
