a27cf9c7642add13fd97d77ecabe1233c6196acf
Unknown <pangtao@mit.edu>
2022-11-23 20:32:38 -0800

    Change paths of quasistatic_simulator_cpp

--- quasistatic_simulator_cpp/src/get_model_paths.cc
+++ quasistatic_simulator_cpp/src/get_model_paths.cc
@@ -8,14 +8,14 @@ using std::filesystem::path;
 std::filesystem::path GetQsimModelsPath() {
   static auto file_path = path(__FILE__);
   static auto q_sim_models_path =
-      file_path.parent_path() / path("../models");
+      file_path.parent_path() / path("../../models");
   return q_sim_models_path;
 }
 
 std::filesystem::path GetRoboticsUtilitiesModelsPath() {
   static auto file_path = path(__FILE__);
   static auto py_package_path =
-      file_path.parent_path() / path("../..");
+      file_path.parent_path() / path("../../..");
   static auto robo_util_models_path = py_package_path /
                                       path("robotics_utilities_pang") /
                                       path("robotics_utilities/models");
