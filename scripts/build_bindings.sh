b27f90a8088307f1df6a85c7b644a5565680ca5f
Pang Tao <pangtao@mit.edu>
2023-02-23 22:48:21 -0500

    Peg drake version to v1.12.0, 20230112.

--- scripts/build_bindings.sh
+++ scripts/build_bindings.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 # build quasistatic_simulator_cpp
 cd $QSIM_CPP_PATH
 mkdir build && cd build
-cmake -DCMAKE_PREFIX_PATH=/opt/drake -DEigen3_DIR:PATH=/usr/local/lib/cmake/eigen3 -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_PREFIX_PATH=/opt/drake -DEigen3_DIR:PATH=/usr/local/lib/cmake/eigen3 -DCMAKE_BUILD_TYPE=Release ..
 make -j 2
